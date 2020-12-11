b1dc801e43711fd8073a65a391f212a0ea7fecc6
Jacob Hein <jacobhein1892@gmail.com>
2020-12-11 02:34:34 +0000

    basic functions

---
+++ functions.js
@@ -0,0 +1,17 @@
+/*Create a function called buildTriangle()
+
+1) get size of triangle
+
+function makeLine(length) {
+  var line = "";
+  for (var j = 1; j <= length; j++) {
+      line += "* ";
+  }
+  return line + "\n";
+}
+
+function buildTriangle(size){
+  for (var i = 1; i <= size; i++){
+    return makeLine(i)
+  }
+}*/
