5ddbe60b26a077084eaac4bd0e70b1d41527bf45
Jacob Hein <jacobhein1892@gmail.com>
2020-12-11 02:34:12 +0000

    hello document

--- hello.js
+++ hello.js
@@ -2,3 +2,20 @@ const sayHello = function(name) {
   console.log("Hello, " + name);
 }
 sayHello('Gabby');
+
+// console.log
+
+const sayHelloToConsole = function(name) {
+  console.log("Hello, " + name);
+}
+
+sayHelloToConsole('Jake'); 
+
+// return
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('John');
+console.log(greeting);
