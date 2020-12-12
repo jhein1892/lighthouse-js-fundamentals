52c02ddf9a687817c1024ab6e44595e161ba027e
Jacob Hein <jacobhein1892@gmail.com>
2020-12-12 02:13:41 +0000

    updated itterations

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,17 @@
+// for loop over an array
+
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+// while loop over an array
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
